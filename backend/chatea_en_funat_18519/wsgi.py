30565b3e3348e5554012578d4d052ef1457f5895
Crowdbotics <team@crowdbotics.com>
2020-06-30 21:43:16 +0000

    Update models

--- backend/chatea_en_funat_18519/wsgi.py
+++ backend/chatea_en_funat_18519/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatea_en_funat_18519.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatea_en_funat_18519.settings")
 
 application = get_wsgi_application()
